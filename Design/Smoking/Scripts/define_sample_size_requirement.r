6b20de3f6dc596a37d17d14c9cb760ac19bbc612
Sergei Manakov <smanakov@illumina.com>
2017-05-26 07:59:18 -0700

    Added plot for another CpG

--- Design/Smoking/Scripts/define_sample_size_requirement.r
+++ Design/Smoking/Scripts/define_sample_size_requirement.r
@@ -1,16 +1,25 @@
 
 # STAGE EXPERIMENTS
 
+# cpgName <- "cg03636183"
+cpgName <- "cg03707168"
+outfi <- paste("Plots/define_sample_size_requirments.", cpgName, ".pdf", sep = '')
+
 # Never smokers (ns): Mean and standard deviation of beta
-ns_mean <- 0.672
-ns_sd <- 0.032
+# ns_mean <- 0.672
+# ns_sd <- 0.032
+ns_mean <- 0.300
+ns_sd <- 0.054
 
 # Current smokers (cs): Mean and standard deviation of beta 
-cs_mean <- 0.553
-cs_sd <- 0.076
+# cs_mean <- 0.553
+# cs_sd <- 0.076
+cs_mean <- 0.263
+cs_sd <- 0.058
 
-# Number of experiments
+# Number of experiments and participants
 nexpr <- 100
+ngroup <- 100
 
 # Run a single experiment
 run_experiment <- function(size, ns_mean, ns_sd, cs_mean, cs_sd)
@@ -27,7 +36,7 @@ run_experiment <- function(size, ns_mean, ns_sd, cs_mean, cs_sd)
 
 # Sizes of test groups to compare
 all_results <- list()
-for (size in c(2:20))
+for (size in c(2:ngroup))
 {
     results <- NULL
     for (experiment in c(1:nexpr))
@@ -38,14 +47,14 @@ for (size in c(2:20))
     all_results[[size]] <- -log10(results)
 }
 
-pdf("Plots/define_sample_size_requirments.pdf", height = 6, width = 10)
+pdf(outfi, height = 6, width = 10)
     boxplot(all_results,
             ylab = "-log10(p.value)", 
     	    xlab = "Sizes of groups of participants to compare",
             range = 0.5,
-    	    main = paste("Outcomes of ", nexpr, " experiments comparing cg03636183 methylation \n",
+    	    main = paste("Outcomes of ", nexpr, " experiments comparing ", cpgName, " methylation \n",
                          "between non-smokers and current smokers\n",
-                         "in groups from 2 to 20 participants"))
+                         "in groups from 2 to ", ngroup, " participants"))
     abline(h = -log10(0.05), col = "darkgreen", lwd = 2)
     abline(h = -log10(0.2), col = "red", lwd = 2)
     legend("topleft", pch = 19, col = "white",
