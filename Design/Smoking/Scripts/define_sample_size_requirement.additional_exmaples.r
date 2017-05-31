2fd047ae8c6b3ec02bb6ae69c67bd911f030d8a8
Sergei Manakov <smanakov@illumina.com>
2017-05-30 23:14:39 -0700

    Jupyter example

--- Design/Smoking/Scripts/define_sample_size_requirement.additional_exmaples.r
+++ Design/Smoking/Scripts/define_sample_size_requirement.additional_exmaples.r
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-
-# Function to run a single experiment
-run_experiment <- function(size, ns_mean, ns_sd, cs_mean, cs_sd)
-{
-    ns_data <- rnorm(size, mean = ns_mean, sd = ns_sd)
-    cs_data <- rnorm(size, mean = cs_mean, sd = cs_sd)
-
-    test <- t.test(ns_data, cs_data)
-    return(test$p.value)
-}
-
-# STAGE EXPERIMENTS
-
-# Never smokers (ns): Mean and standard deviation of beta
-ns_mean <- 0.672
-ns_sd <- 0.032
-
-# Current smokers (cs): Mean and standard deviation of beta 
-cs_mean <- 0.553
-cs_sd <- 0.076
-
-# Number of experiments
-nexpr <- 100
-
-# Sizes of test groups to compare
-all_results <- list()
-for (size in c(2:20))
-{
-    results <- NULL
-    for (experiment in c(1:nexpr))
-    {
-        results <- c(results, run_experiment(size, ns_mean, ns_sd, cs_mean, cs_sd))
-    }
-
-    all_results[[size]] <- -log10(results)
-}
-
-all_results
-
-# set dimensions of the plot area
-library(repr)
-options(repr.plot.width = 10, repr.plot.heigh = 6)
-
-# plot
-boxplot(all_results,
-        ylab = "-log10(p.value)", 
-        xlab = "Sizes of groups of participants to compare",
-        range = 0.5,
-        main = paste("Outcomes of ", nexpr, " experiments comparing cg03636183 methylation \n",
-                         "between non-smokers and current smokers\n",
-                         "in groups from 2 to 20 participants"))
-abline(h = -log10(0.05), col = "darkgreen", lwd = 2)
-abline(h = -log10(0.2), col = "red", lwd = 2)
-legend("topleft", pch = 19, col = "white",
-       legend = c(paste("Never-smokers mean methylation: ", 100*ns_mean, "%", sep = ""),
-                  paste("Never-smokers standard deviation: ", ns_sd, sep = ""),
-                  paste("Current smokers mean methylation: ", 100*cs_mean, "%", sep = ""),
-                  paste("Current smokers standard deviation: ", cs_sd, sep = "")),
-       bty = "n")
-
-
-=======
 
 # STAGE EXPERIMENTS
 
@@ -130,4 +67,3 @@ pdf(outfi, height = 6, width = 10)
 dev.off()
 
 
->>>>>>> 6b20de3f6dc596a37d17d14c9cb760ac19bbc612
